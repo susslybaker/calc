9fda61599ce408121484aae5bd869d47e7758dd3
susslybaker <130255549+susslybaker@users.noreply.github.com>
2023-04-18 21:32:21 +0400

    git commit

--- Calc/TestProject2/UnitTest1.cs
+++ Calc/TestProject2/UnitTest1.cs
@@ -24,7 +24,7 @@ namespace TestCalc
 
             string oper = "/";
 
-            string expected = "Error";
+            string expected = "∞";
 
             //Act
             string actual = MyCalc.CountUp(oper, left, right);
@@ -38,71 +38,6 @@ namespace TestCalc
 
         }
 
-        [Test]
-        public void LastIndexIsZero()
-        {
-            //Arrange
-            int str = 15;
-
-            string expected = "0";
-
-            //Act
-            string actual = MyCalc.IndexLast(str);
-
-            //Assert
-            Assert.That(actual, Is.EqualTo(expected));
-
-        }
-
-        [Test]
-
-        public void CtringCalculatorWithoutValues()
-        {
-            //Arrange
-            string calcstr = "StrWithOutVal";
-
-            string expected = "ErrorValue";
-            //Act
-            string actual = MyCalc.Calculate(calcstr);
-            //Assert
-            Assert.That(actual, Is.EqualTo(expected));
-
-        }
-
-        [Test]
-
-        public void LastIndexsIsZero2()
-        {
-            //Arrange
-            int index = 0;
-
-            string expected = "NoNe";
-            //Act
-            string actual = MyCalc.IndexLast(index);
-            //Assert
-            Assert.That(actual, Is.EqualTo(expected));
-
-
-        }
-
-        [Test]
-
-        public void NotOperationINeXample()
-        {
-            //Arrange
-            string left = "4";
-
-            string right = "0";
-
-            string oper = "ThisIsNotAnOp";
-
-            string expected = "Error";
-
-            //Act
-            string actual = MyCalc.CountUp(oper, left, right);
-            //Assert
-            Assert.That(actual, Is.EqualTo(expected));
-        }
 
         [Test]
         public void TestAddition()
